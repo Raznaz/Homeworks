b1635df6a9884f99bc66e2c06c8eae8e176bc4dc
Naz <nazzzarr@gmail.com>
2021-07-28 11:35:58 +0300

    task 1 finish

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('test');
+// HW1
+
+// Task 1
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) {
+    console.log(i, 'FizBuz');
+  } else if (i % 2 === 0) {
+    console.log(i, 'Fiz');
+  } else {
+    console.log(i, 'Buz');
+  }
+}
