d259cdfaf236e618793e66d0eb76469cfe087240
Naz <nazzzarr@gmail.com>
2021-07-31 22:00:50 +0300

    Task 1 finish

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 //  HW2
 
-// test test test test test test
+// Task 1
+
+const citiesAndCountries = {
+  Киев: 'Украина',
+  'Нью-Йорк': 'США',
+  Амстердам: 'Нидерланды',
+  Берлин: 'Германия',
+  Париж: 'Франция',
+  Лиссабон: 'Португалия',
+  Вена: 'Австрия'
+};
+
+const showCapitalOfCountries = obj => {
+  const arrCapAndCountr = [];
+  for (const key in obj) {
+    arrCapAndCountr.push(`${key} - это ${obj[key]}`);
+  }
+  return arrCapAndCountr;
+};
+
+console.log(showCapitalOfCountries(citiesAndCountries));
