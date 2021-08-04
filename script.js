6a59e8e46d90224154e62f46e108bc498643fb21
Naz <nazzzarr@gmail.com>
2021-08-04 16:45:31 +0300

    add file emploee.js and Task 1

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 /* 
 test test test
 */
+// HW3
+
+// Task 1
+
+function Emploee(emploee) {
+  this.id = emploee.id;
+  this.name = emploee.name;
+  this.surname = emploee.surname;
+  this.salary = emploee.salary;
+  this.workExperience = emploee.workExperience;
+  this.isPrivileges = emploee.Privileges;
+  this.gender = emploee.gender;
+}
+
+const emploee = emplyeeArr[0];
+const employeeObj = new Emploee(emploee);
