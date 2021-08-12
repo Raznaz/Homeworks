1c77d2c0edda2e40ac7e05636118a08bb9dca720
Naz <nazzzarr@gmail.com>
2021-08-12 13:48:33 +0300

    fix bugs

--- script.js
+++ script.js
@@ -117,7 +117,9 @@ class Validator {
   }
 
   checkIsDomain(domain) {
-    let res = domain.match(/^((http|https):\/\/)?(www\.)?\w+(\.\w{2,3})+$/g);
+    let res = domain.match(
+      /^((http|ftp|https):\/\/)?(www\.)?[\w-]+(\.\w{2,3})+$/g
+    );
     if (res == null) {
       return false;
     } else {
