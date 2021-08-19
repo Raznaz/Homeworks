13cca63092f10663f355ab0c7ae70c23a7c3309b
Naz <nazzzarr@gmail.com>
2021-08-19 14:11:36 +0300

    add file candidates, connect in index.html, Task 1 - 9

--- script.js
+++ script.js
@@ -1,1 +1,144 @@
-console.log('test');
+// HW 6
+
+//  Task 1
+
+const arr = ['Vasya', 'Petya', 'Alexey'];
+
+const removeUser = function (arr, index) {
+  return arr.splice(index, 1);
+};
+
+// console.log(removeUser(arr, 1));
+// console.log(arr);
+
+//  Task 2
+
+const obj = { name: 'Vasya', age: 1 };
+
+const getAllKeys = function (obj) {
+  const arrKeys = [];
+  for (const key in obj) {
+    arrKeys.push(key);
+  }
+  return arrKeys;
+};
+
+getAllKeys(obj); /// ["name", "age"]
+// console.log(getAllKeys(obj));
+
+// Task 3
+
+const getAllValues = function (obj) {
+  const arrValues = [];
+  for (const key in obj) {
+    arrValues.push(obj[key]);
+  }
+  return arrValues;
+};
+
+// console.log(getAllValues(obj));
+
+// Task 4
+
+const firstObj = {
+  id: 777,
+  name: 'VASYA',
+};
+
+const secondObj = {
+  id: 888,
+  name: 'KATYA',
+};
+
+const insertIntoArr = function (object, idCandidate) {
+  let myIndex = null;
+  condidateArr.forEach((obj, index) => {
+    if (obj._id === idCandidate) {
+      myIndex = index;
+    }
+  });
+  return condidateArr.splice(myIndex, 0, object);
+};
+
+// insertIntoArr(firstObj, '5e216bc941925a438f823873');
+// insertIntoArr(secondObj, '5e216bc9a9a703233fd5ef11');
+// console.log(condidateArr);
+
+// Task 5
+
+class Condidate {
+  constructor(obj) {
+    this.obj = obj;
+  }
+
+  state() {
+    let addressArr = this.obj.address.split(', ');
+    return addressArr[2];
+  }
+}
+
+const condidate = new Condidate(condidateArr[40]);
+// const condidate = new Condidate(condidateArr[0]);
+
+// Task 6
+
+const getCompanyNames = function () {
+  companyNamesArr = [];
+  condidateArr.forEach(obj => {
+    if (obj.company) companyNamesArr.push(obj.company);
+  });
+
+  return [...new Set(companyNamesArr)];
+};
+
+getCompanyNames();
+
+// Task 7
+
+const getUsersByYear = function (year) {
+  const idArr = [];
+  condidateArr.forEach(obj => {
+    if (year === new Date(obj.registered).getFullYear()) {
+      idArr.push(obj._id);
+    }
+  });
+
+  return idArr;
+};
+
+// console.log(getUsersByYear(2015));
+// console.log(getUsersByYear(2019));
+
+// Task 8
+
+const getCondidatesByUnreadMsg = function (unreadMsg) {
+  const arrInstanceCand = [];
+  const regEx = /\d+/;
+  condidateArr.forEach(obj => {
+    let unMsg = +regEx.exec(obj.greeting);
+    if (unMsg === unreadMsg) {
+      arrInstanceCand.push(new Condidate(obj));
+    }
+  });
+  return arrInstanceCand;
+};
+
+// console.log(getCondidatesByUnreadMsg(8));
+// console.log(getCondidatesByUnreadMsg(6));
+
+// Task 9
+
+const getCondidatesByGender = function (gen) {
+  const arrInstanceCandByGen = [];
+  condidateArr.forEach(obj => {
+    if (obj.gender === gen) {
+      arrInstanceCandByGen.push(new Condidate(obj));
+    }
+  });
+  return arrInstanceCandByGen;
+};
+
+console.log(getCondidatesByGender('male'));
+console.log(getCondidatesByGender('female'));
+
+// Task 10
