46bf2dec4e5702cc39cfb4cacd5f4b1273dab80d
Naz <nazzzarr@gmail.com>
2021-08-31 10:21:29 +0300

    start Task 2

--- script.js
+++ script.js
@@ -2,22 +2,28 @@
 
 // Task 1
 
-const form = document.querySelector('form');
-let number = +prompt('Введите число', 0);
+// const form = document.querySelector('form');
+// let number = +prompt('Введите число', 0);
 
-for (let i = number; i >= 1; i--) {
-  const inp = document.createElement('input');
-  inp.classList.add('input-item');
-  inp.value = `Input ${i}`;
-  if (i % 2) {
-    inp.classList.add('input-bg');
-  }
-  if (!(i % 3)) {
-    inp.value = null;
-    inp.placeholder = 'some text for each third';
-  }
-  if (i === number) {
-    inp.classList.add('margin-zero');
-  }
-  form.prepend(inp);
-}
+// for (let i = number; i >= 1; i--) {
+//   const inp = document.createElement('input');
+//   inp.classList.add('input-item');
+//   inp.value = `Input ${i}`;
+//   if (i % 2) {
+//     inp.classList.add('input-bg');
+//   }
+//   if (!(i % 3)) {
+//     inp.value = null;
+//     inp.placeholder = 'some text for each third';
+//   }
+//   if (i === number) {
+//     inp.classList.add('margin-zero');
+//   }
+//   form.prepend(inp);
+// }
+
+// Task 2
+
+const h2 = document.createElement('h2');
+h2.innerHTML = '00:30:59';
+body.document.append('h2');
