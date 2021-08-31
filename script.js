ea46b4196221f60ee02dff9aee1e33d61501ed67
Naz <nazzzarr@gmail.com>
2021-08-31 20:00:25 +0300

    Task 3, Task 4

--- script.js
+++ script.js
@@ -2,27 +2,25 @@
 
 // Task 1
 
-// const form = document.querySelector('form');
-// let number = +prompt('Введите число', 0);
+const form = document.querySelector('form');
+let number = +prompt('Введите число', 0);
 
-// for (let i = number; i >= 1; i--) {
-//   const inp = document.createElement('input');
-//   inp.classList.add('input-item');
-//   inp.value = `Input ${i}`;
-//   if (i % 2) {
-//     inp.classList.add('input-bg');
-//   }
-//   if (!(i % 3)) {
-//     inp.value = null;
-//     inp.placeholder = 'some text for each third';
-//   }
-//   if (i === number) {
-//     inp.classList.add('margin-zero');
-//   }
-//   form.prepend(inp);
-// }
-
-// Task 2
+for (let i = number; i >= 1; i--) {
+  const inp = document.createElement('input');
+  inp.classList.add('input-item');
+  inp.value = `Input ${i}`;
+  if (i % 2) {
+    inp.classList.add('input-bg');
+  }
+  if (!(i % 3)) {
+    inp.value = null;
+    inp.placeholder = 'some text for each third';
+  }
+  if (i === number) {
+    inp.classList.add('margin-zero');
+  }
+  form.prepend(inp);
+}
 
 // Task 2
 const sec = 4000;
@@ -87,4 +85,62 @@ btnStop.addEventListener('click', () => {
 
 btnStart.addEventListener('click', () => {
   timer(pause);
+});
+
+// Task 3
+
+const changeColorLastParagraph = selector => {
+  const p = document.querySelectorAll(selector);
+
+  p.forEach((paragraph, index) => {
+    if (!p[index + 1]) {
+      console.log(index);
+      p[index].style.backgroundColor = 'red';
+    }
+  });
+};
+
+changeColorLastParagraph('p');
+
+const changeFooterToMain = () => {
+  const wrapper = document.getElementById('wrapper');
+  const footer = document.getElementById('footer');
+  const main = document.getElementById('main');
+
+  wrapper.append(footer);
+};
+
+changeFooterToMain();
+
+// Task 4
+
+const INGREDIENTS = {
+  cocoa: ['cocoa powder', 'milk', 'sugar'],
+  cappuccino: ['milk', 'coffee'],
+  smoothie: ['banana', 'orange', 'sugar'],
+  'matcha frappe': ['matcha', 'milk', 'ice'],
+};
+
+const menu = document.getElementById('menu');
+
+const showMenu = (elem, elemContent) => {
+  if (elem.dataset.show === 'true') {
+    elem.removeChild(elem.lastChild);
+    elem.dataset.show = false;
+  }
+  if (INGREDIENTS[elemContent]) {
+    const ol = document.createElement('ol');
+    elem.append(ol);
+    elem.setAttribute('data-show', true);
+    for (let i = 0; i <= INGREDIENTS[elemContent].length - 1; i++) {
+      const li = document.createElement('li');
+      li.textContent = INGREDIENTS[elemContent][i];
+      ol.append(li);
+    }
+  }
+};
+
+menu.addEventListener('click', event => {
+  target = event.target;
+  showMenu(target, target.textContent);
 });
