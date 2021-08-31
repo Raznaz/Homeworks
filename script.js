f573a57f179cc8ac60246d04dc658904b35084da
Naz <nazzzarr@gmail.com>
2021-08-31 10:15:32 +0300

    add style, Task 1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('test');
+// HW8
+
+// Task 1
+
+const form = document.querySelector('form');
+let number = +prompt('Введите число', 0);
+
+for (let i = number; i >= 1; i--) {
+  const inp = document.createElement('input');
+  inp.classList.add('input-item');
+  inp.value = `Input ${i}`;
+  if (i % 2) {
+    inp.classList.add('input-bg');
+  }
+  if (!(i % 3)) {
+    inp.value = null;
+    inp.placeholder = 'some text for each third';
+  }
+  if (i === number) {
+    inp.classList.add('margin-zero');
+  }
+  form.prepend(inp);
+}
