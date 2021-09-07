023372100e190fd307fad25b526cacd06f6baa66
Naz <nazzzarr@gmail.com>
2021-09-07 09:52:21 +0300

    Task 1 refactoring

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
 // HW 10 Promise
 
 // Task 1
-
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const rndNum = Math.floor(Math.random() * (7 - 1) + 1);
+    const rndNum = Math.floor(Math.random() * 6) + 1;
+    console.log('Случайное число', rndNum);
     if (rndNum >= 1 && rndNum <= 5) {
       console.log('Start the game...');
       resolve(rndNum);
     } else {
       reject();
     }
-  }, 2000);
+  }, 20);
 })
   .then((number) => {
+    console.log(number);
     if (number === 1) {
       console.log('Stay here');
     } else {
       console.log(`Go ${number} steps`);
     }
   })
-  .catch((err) => {
-    console.error('My Error !', err);
-  })
-  .finally(() => {
-    console.log('Exit');
+  .catch(() => {
+    console.error('Exit');
   });
 
 // Task 2
